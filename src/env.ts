7c73e43892022beb2b4cf568a8a3e00079918bb5
puru <butackle@gmail.com>
2023-07-09 22:55:23 +0900

    refactor

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,3 @@
-import * as path from 'path'
 import 'dotenv/config'
 
 const Num = (num: any) => {
@@ -13,7 +12,6 @@ const DEV_MODE: Readonly<boolean> = process.env.NODE_ENV === 'development';
 
 const PORT: Readonly<number> = Num(process.env.PORT) ?? 23344;
 const PING_MSG: Readonly<string> = process.env.PING_MSG ?? 'Hello World!!!!!';
-const LOG_PATH: Readonly<string> = process.env.LOG_PATH ?? path.join(__dirname, '../log/shellgeiBot.log');
 
 // missKey
 const MISS_KEY_HOST: Readonly<string> = process.env.MISS_KEY_HOST ?? '';
@@ -26,7 +24,6 @@ export {
   DEV_MODE,
   PORT,
   PING_MSG,
-  LOG_PATH,
   MISS_KEY_HOST,
   MISS_KEY_MENTION_SECRET,
   MISS_KEY_CREATE_NOTE_API_KEY,
